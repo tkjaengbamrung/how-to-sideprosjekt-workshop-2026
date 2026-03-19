8960b3fa87f185cf990106022307d7c0bcaf847e
Tan.Jaengbamrung <tajae9425@uib.no>
2026-03-19 18:00:58 +0100

    added lovely picture of my friend

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@
 export default function Home() {
   return (
     <center>
-      <div>
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <h1> Hei, jeg heter Tan </h1>
         <p> Jeg studerer Datateknologi på Universitetet i Bergen </p>
+        <p> Dette er vennen min Haron </p>
+        <img 
+          src="/haron.jpg"
+          width={100}
+          height={20}
+        />
       </div>
     </center>
   );
